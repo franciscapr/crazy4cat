c8d552acd5b8b4d1b5d2bcd9e9eb1d36e9bcc088
franciscapr <francisca.paredes.pr@gmail.com>
2023-07-17 17:49:03 +0000

    subiendo

---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :reactions, dependent: :destroy
+  accepts_nested_attributes_for :comments, reject_if: :no_name_category
+
+  Kinds = %w[like dislike].freeze
+
+  attribute :when_went, :datetime
+end
